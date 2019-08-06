48b47700d606fcc6adf8cf7231aff91cba79fe01
Arunkumaran K E <35808027+Arunkumaranke@users.noreply.github.com>
2019-08-06 19:13:44 +0530

    Create no_of_lines.cpp

---
+++ code-3/no_of_lines.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string a;
+    int count=1;
+    getline(cin,a);
+    for(int i=0; a[i] != 0;i++)
+    {
+        if(a[i] == '.')
+            count++;
+        
+    }
+    printf("%d", count);
+    return 0;
+}
+    
