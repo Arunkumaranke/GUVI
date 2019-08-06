d6c54f84bf5b5562e6ece85de657641734533ffe
Arunkumaran K E <35808027+Arunkumaranke@users.noreply.github.com>
2019-08-06 18:46:43 +0530

    Create space-exception.cpp

---
+++ code-3/space-exception.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char a[100];
+    int count=0;
+    gets(a);
+    for(int i=0; a[i] != 0;i++)
+    {
+        if(a[i] != ' ')
+            count++;
+        
+    }
+    printf("%d", count);
+    return 0;
+}
