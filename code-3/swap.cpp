0221e7f1657e8eeda8fafcbd53bb1829b93f9332
Arunkumaran K E <35808027+Arunkumaranke@users.noreply.github.com>
2019-08-07 10:21:07 +0530

    Create swap.cpp

---
+++ code-3/swap.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+    int ab,bc,ca;
+    cin>>ab>>bc;
+    ca=ab;
+    ab=bc;
+    bc=ca;
+    cout<<ab<<" "<<bc;
+    return 0;
+}
+    
